f56d7dd7e252d9dec2d9fc8b923b2f599d8cf812
jon4h <admin@psytux.com>
2021-04-29 01:58:20 +0200

    use interface and type switch

--- utils/error.go
+++ utils/error.go
@@ -6,16 +6,30 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func NewHTTPError(ctx *fiber.Ctx, status int, err error) {
-	er := HTTPError{
-		Code:    status,
-		Message: err.Error(),
+func NewHTTPError(ctx *fiber.Ctx, status int, err interface{}) error {
+	var er *HTTPError
+	switch err := err.(type) {
+	case error:
+		er = &HTTPError{
+			Code:    status,
+			Message: err.Error(),
+		}
+	case string:
+		er = &HTTPError{
+			Code:    status,
+			Message: err,
+		}
+	default:
+		er = &HTTPError{
+			Code:    status,
+			Message: fmt.Sprintf("%s", err),
+		}
 	}
 
 	if err := ctx.Status(status).JSON(er); err != nil {
-		ctx.Status(500).SendString(fmt.Sprintf("%s", err))
-		return
+		return ctx.Status(500).SendString(fmt.Sprintf("%s", err))
 	}
+	return nil
 }
 
 type HTTPError struct {
