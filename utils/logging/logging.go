e969bbcc377005ac535116911550c8d8d34bd4c8
jon4h <admin@psytux.com>
2021-04-29 02:00:15 +0200

    logging initialization

--- utils/logging/logging.go
+++ utils/logging/logging.go
@@ -2,26 +2,35 @@ package logging
 
 import (
 	"os"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 )
 
 var (
-	Log *log.Logger
+	Log = log.New()
 )
 
 type Config struct {
-	Debug bool `yaml:"debugging"`
+	LogLevel string `mapstructure:"logLevel"`
 }
 
-func Init() {
-	Log = log.New()
+// Initalize a new logger.
+//You can set a different logging level by useing the logging.logLevel YAML key or using the LOG_LEVEL env variable.
+// By default the logging level is set to "info" but it can be changed to either "warn" or "debug"
+func Init(cfg *Config) {
 
+	Log.SetOutput(os.Stdout)
 	Log.SetFormatter(&log.TextFormatter{
 		FullTimestamp: true,
 	})
 
-	Log.SetOutput(os.Stdout)
-	Log.SetLevel(log.WarnLevel)
+	if strings.EqualFold(cfg.LogLevel, "Info") {
+		Log.SetLevel(log.InfoLevel)
+	} else if strings.EqualFold(cfg.LogLevel, "Warn") {
+		Log.SetLevel(log.WarnLevel)
+	} else if strings.EqualFold(cfg.LogLevel, "Debug") {
+		Log.SetLevel(log.DebugLevel)
+	}
 
 }
