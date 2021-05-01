25017f7b188a80e7afdf1af158b9739e4f34be4d
jon4h <admin@psytux.com>
2021-05-01 18:07:01 +0200

    enable foreign key constraints for sqlite

--- config/database/database.go
+++ config/database/database.go
@@ -18,7 +18,8 @@ type Config struct {
 
 func Connect(config *Config) {
 	var err error
-	var gormConfig = &gorm.Config{QueryFields: true}
+	//var gormConfig = &gorm.Config{QueryFields: true}
+	var gormConfig = &gorm.Config{}
 
 	if !config.Debug {
 		gormConfig.Logger = logger.Default.LogMode(logger.LogLevel(0))
@@ -29,6 +30,9 @@ func Connect(config *Config) {
 	if err != nil {
 		panic(fmt.Sprintf("[database][init] error: %s", err))
 	}
+
+	// enable foreign key constraints
+	DB.Exec("PRAGMA foreign_keys = ON;")
 }
 
 func Migrate(tables ...interface{}) error {
