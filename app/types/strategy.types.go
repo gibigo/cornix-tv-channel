a4f35b6e8f73c11d5171d83cff869fd41dac4b4b
jon4h <admin@psytux.com>
2021-05-05 22:23:27 +0200

    add json tags

--- app/types/strategy.types.go
+++ app/types/strategy.types.go
@@ -1,88 +1,52 @@
 package types
 
 type Strategy struct {
-	ID             uint `json:"-"`
-	AllowCounter   bool
-	Symbol         string
-	TargetStrategy *TargetStrategy
-	ZoneStrategy   *ZoneStrategy
-	ChannelID      uint `json:"-"`
+	ID             uint            `json:"-"`
+	AllowCounter   bool            `json:"allowCounter"`
+	Symbol         string          `json:"symbol"`
+	TargetStrategy *TargetStrategy `json:"targetStrategy,omitempty"`
+	ZoneStrategy   *ZoneStrategy   `json:"zoneStrategy,omitempty"`
+	ChannelID      uint            `json:"-"`
 }
 
 type ZoneStrategy struct {
-	ID         uint `json:"-"`
-	EntryStart float64
-	EntryStop  float64
-	TPs        []*TP
-	SL         *SL
-	IsBreakout bool
-	StrategyID uint `json:"-"`
+	ID         uint    `json:"-"`
+	EntryStart float64 `json:"entryStart"`
+	EntryStop  float64 `json:"entryStop"`
+	TPs        []*TP   `json:"tps"`
+	SL         *SL     `json:"sl"`
+	IsBreakout bool    `json:"isBreakout"`
+	StrategyID uint    `json:"-"`
 }
 
 type TargetStrategy struct {
-	ID         uint `json:"-"`
-	Entries    []*Entry
-	TPs        []*TP
-	SL         *SL
-	IsBreakout bool
-	StrategyID uint `json:"-"`
+	ID         uint     `json:"-"`
+	Entries    []*Entry `json:"entries"`
+	TPs        []*TP    `json:"tps"`
+	SL         *SL      `json:"sl"`
+	IsBreakout bool     `json:"isBreakout"`
+	StrategyID uint     `json:"-"`
 }
 
 type Entry struct {
-	ID               uint `json:"-"`
-	Diff             float64
-	TargetStrategyID uint `json:"-"`
+	ID               uint    `json:"-"`
+	Diff             float64 `json:"diff"`
+	TargetStrategyID uint    `json:"-"`
 }
 
 type TP struct {
-	ID               uint `json:"-"`
-	Diff             float64
-	TargetStrategyID uint `json:"-"`
-	ZoneStrategyID   uint `json:"-"`
+	ID               uint    `json:"-"`
+	Diff             float64 `json:"diff"`
+	TargetStrategyID uint    `json:"-"`
+	ZoneStrategyID   uint    `json:"-"`
 }
 
 type SL struct {
-	ID               uint `json:"-"`
-	Diff             float64
-	TargetStrategyID uint `json:"-"`
-	ZoneStrategyID   uint `json:"-"`
+	ID               uint    `json:"-"`
+	Diff             float64 `json:"diff"`
+	TargetStrategyID uint    `json:"-"`
+	ZoneStrategyID   uint    `json:"-"`
 }
-
-/* type Strategy struct {
-	AllowCounter   bool
-	Symbol         string
-	TargetStrategy *TargetStrategy
-	ZoneStrategy   *ZoneStrategy
-}
-
-type ZoneStrategy struct {
-	EntryStart float64
-	EntryStop  float64
-	TPs        []*TP
-	SL         *SL
-	IsBreakout bool
-}
-
-type TargetStrategy struct {
-	ID         uint
-	Entries    []*Entry
-	TPs        []*TP
-	SL         *SL
-	IsBreakout bool
-}
-
-type Entry struct {
-	Diff             float64
-	TargetStrategyID uint
-}
-
-type TP struct {
-	Diff float64
-}
-
-type SL struct {
-	Diff float64
-} */
 
 type AddStrategy struct {
 	Symbol         string          `json:"symbol" validate:"required"`
