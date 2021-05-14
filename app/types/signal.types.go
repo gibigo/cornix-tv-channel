e99f0c5cdeae2edc04ebb48be22ac07c01a59f56
jon4h <admin@psytux.com>
2021-05-14 23:31:43 +0200

    add validator, rename ticker to symbol inside the database

--- app/types/signal.types.go
+++ app/types/signal.types.go
@@ -1,10 +1,10 @@
 package types
 
 type TVSignal struct {
-	Ticker    string  `json:"ticker"`
-	Price     float64 `json:"price"`
-	ChannelID int64   `json:"TGChannel"`
+	Symbol    string  `json:"ticker" validate:"required"`
+	Price     float64 `json:"price" validate:"required"`
+	ChannelID int64   `json:"TGChannel" validate:"required"`
 	Direction string  `json:"direction,omitempty"`
-	User      string  `json:"user"`
-	UUID      string  `json:"uuid"`
+	User      string  `json:"user" validate:"required"`
+	UUID      string  `json:"uuid" validate:"required"`
 }
