9c0bb8fce7c84e79c4818bfb3a454670821e9002
jon4h <admin@psytux.com>
2021-05-16 02:55:07 +0200

    forward webhook signals to telegram

--- app/dal/signal.dal.go
+++ app/dal/signal.dal.go
@@ -10,9 +10,18 @@ type TVSignal struct {
 	Symbol    string
 	Price     float64
 	Direction string
+	Exchange  string
 	ChannelID uint
 }
 
 func FindSignalBySymbol(dest interface{}, channelID uint, symbol string) *gorm.DB {
 	return database.DB.Where("channel_id = ? AND symbol = ?", channelID, symbol).First(dest)
+}
+
+func CreateSignal(s *TVSignal) *gorm.DB {
+	return database.DB.Create(s)
+}
+
+func DeleteSignal(channelID uint, symbol string) *gorm.DB {
+	return database.DB.Where("channel_id = ? AND symbol = ?", channelID, symbol).Delete(&TVSignal{})
 }
